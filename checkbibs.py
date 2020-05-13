2a10ffc3a6b1c04de947f0832fad8e68df78d348
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-05-13 13:07:52 -0600

    add a script to check if my bibfile is broken and added a bunch of new papers to start reading

---
+++ checkbibs.py
@@ -0,0 +1,37 @@
+import subprocess as sp
+
+
+def tag_only(line):
+    """get just the tag from the line with '@article'"""
+    start = line.index("{")
+    end = line.index(",")
+    return line[start+1:end]
+
+def main():
+    """I'm always worried that Mendeley is losing my papers.
+    This script is to give me some peace of mind that
+    my bibs aren't disappearing before I commit the new bib file.
+    """
+    command = ["git", "diff", "library.bib"]
+    p = sp.run(command, capture_output=True)
+    lines = p.stdout.decode("utf-8").split("\n")
+    tags = [line for line in lines if "@article" in line]
+    plus = {tag_only(tag) for tag in tags if tag[0] == "+"}
+    minus = {tag_only(tag) for tag in tags if tag[0] == "-"}
+    if minus.issubset(plus):
+        print("All good! No bibs missing")
+    else:
+        missing = []
+        check = minus - plus
+        for tag in check:
+            # check if just an 'a', 'b' got added/removed etc
+            if tag[:-1] not in plus:
+                missing.append(tag)
+        if missing:
+            print("These bibs were missing: ", missing)
+        else:
+            print("All good! No bibs missing")
+
+
+if __name__ == "__main__":
+    main()
